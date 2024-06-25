a7d91c8b34f9eeee82d2aaae4e190944d4a4c626
Paulo Henderson Miranda da Silva <131710863+MirandaPHY@users.noreply.github.com>
2024-06-25 11:43:26 -0300

    loops

--- Etapa1/JS/loops.js
+++ Etapa1/JS/loops.js
@@ -1,19 +1,56 @@
-// Estrutura de repetição = loops = laços
+// Estruturas de repetição = loops = laços
 // Essas estruturas possuem:
-  // Condição de parada
-  // Valor inicial ( variavel de controle, contador )
-  // Atualização 
+    // Condição de parada
+    // Valor inicial (variável de controle, contador)
+    // Atualização
 
-  // loop infinito
-  // while(true{
-  //   console.log('Batata')})
+// While = Enquanto
+/*
+    while (condicao) {
+        // o código que será repetido
+    }
+*/
 
-  let numero = 0;
-  //Condição de parada
-  while(numero < 10){
+// Loop infinito
+// while(true) {
+//     console.log("Batata");
+// }
+
+let numero = 0; // Valor inicial
+
+// Condição de parada
+while(numero < 10) {
     console.log(numero);
-    //Atualização
-    numero++;
-  }
+    numero++; // Atualização
+}
 
-  console.log("FIM");
+let numero2 = 0;
+
+while(numero2 <= 50) {
+    console.log(numero2);
+    numero2 += 5; // numero2 = numero2 + 5
+}
+
+let numero3 = 100;
+
+while(numero3 >= 0) {
+    console.log(numero3);
+    numero3--; // numero3 = numero3 - 1
+}
+console.log("WHILE FIM")
+console.log(numero3);
+
+// Soma 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
+let cont = 1;
+let soma = 0; // acumulador
+
+// Do 1 até 10
+while(cont <= 10) {
+    soma = soma + cont;
+    cont++;
+}
+
+console.log(soma);
+
+
+console.log("FIM");

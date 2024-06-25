9d0834b27dfe19ff9b926f0033677a3caa2c2925
Paulo Henderson Miranda da Silva <131710863+MirandaPHY@users.noreply.github.com>
2024-06-25 10:49:06 -0300

    atualizando

---
+++ Etapa1/JS/loops.js
@@ -0,0 +1,19 @@
+// Estrutura de repetição = loops = laços
+// Essas estruturas possuem:
+  // Condição de parada
+  // Valor inicial ( variavel de controle, contador )
+  // Atualização 
+
+  // loop infinito
+  // while(true{
+  //   console.log('Batata')})
+
+  let numero = 0;
+  //Condição de parada
+  while(numero < 10){
+    console.log(numero);
+    //Atualização
+    numero++;
+  }
+
+  console.log("FIM");
